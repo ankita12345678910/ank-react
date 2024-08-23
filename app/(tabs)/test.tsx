245bf8c0722b77c76ab00f620b5fc0e460c5fb38
ankita12345678910 <96244331+ankita12345678910@users.noreply.github.com>
2024-08-23 22:05:48 +0530

    practice-3

--- app/(tabs)/test.tsx
+++ app/(tabs)/test.tsx
@@ -1,5 +1,5 @@
+import React, { useRef, useEffect } from 'react';
 import Example from '../../components/Example'; // Use the correct relative path
-
 
 const Test = () => {
     const obj = {
@@ -7,14 +7,34 @@ const Test = () => {
         age: 20,
         roll: 10,
         college: "xyz"
-    }
+    };
+
     function btnClick() {
-        alert("Heloo i m ankita");
+        alert("Hello, I'm Ankita");
     }
+
+    var demo = useRef(null);
+    var myImage = useRef(null);
+    const change=()=>{
+            // demo.current.innerText = "This is a hook function";
+            demo.innerText = "This is a hook";
+            // document.getElementById('abc').innerText="ppppppppppppppppppp"
+    }
+    const imageChange=()=>{
+        myImage.current.src = "https://placehold.co/600x400?text=Hello\nWorld";
+    }
+
     return (
         <div>
-            <Example title="this is a computer" item={obj} func={btnClick} />
+            <img ref={myImage} src="https://placehold.co/600x400" alt="" />
+            <button onClick={imageChange}></button>
+            <Example title="This is a computer" item={obj} func={btnClick} />
+            <h1 ref={(xyz)=>demo=xyz}>jii</h1>
+            {/* <h1 id='abc' ref={(xy)=>demo=xy}>hee</h1> */}
+            <button onClick={change}>change</button>
+
         </div>
     );
 };
+
 export default Test;
